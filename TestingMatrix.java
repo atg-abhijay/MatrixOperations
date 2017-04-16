0b4b1a25524ab3250f48c51cc7b4cf0bbfbee6ab
atg-abhijay <atg.abhijay@gmail.com>
2017-04-16 01:21:00 -0400

    tested sorting code and it works

--- TestingMatrix.java
+++ TestingMatrix.java
@@ -1,46 +1,9 @@
 import java.util.*;
 public class TestingMatrix {
-  public static void main(String[] args) {
-    //double[][] entries = {{0,1,0}, {0,0,1}, {1,0,0}};
-    /* Matrix A = new Matrix("A", "4x3", true);
-    Matrix B = new Matrix("B", "3x4", true);
+  public static void main(String[] args) {    
+    Matrix A = new Matrix("A", "5x4", true);
     A.printMatrix();
-    B.printMatrix();
-    Matrix product = A.multiplyMatrices(B, "AB");
-    product.printMatrix(); */
-    /* Scanner reader = new Scanner(System.in);
-    String initialDisplay = "Welcome to the program that lets you work with Matrices!";
-    System.out.println(initialDisplay + "\n");
-    String[] options = new String[6];
-    options[0] = "The list of available methods are:\n 1. printMatrix()\n 2. addMatrices(Matrix secondMatrix, String name, boolean subtract)";
-    options[1] = " 3. multiplyByNumber(double constant)\n 4. makeIdentity(int size)";
-    options[2] = " 5. raiseToPower(int power)\n 6. multiplyMatrices(Matrix secondMatrix, String productName)";
-    options[3] = " 7. int determinant()\n 8. void solutionOfSystem(Matrix constantVals)";
-    options[4] = " 9. inverse()\n 10. rowReduce()\n 11. void transpose()";
-    options[5] = " 12. Matrix compare(double value)\n 13. add(String rowOrColumn, int position)\n 14. setName(String newName)";
-    for(int i = 0; i < options.length; i++) {
-      System.out.println(options[i]);
-    } */
-    /* while(!reader.next().equals("exit")) {
-      System.out.println("Random matrix or not? (true/false)");
-      boolean random = Boolean.parseBoolean(reader.next());
-      System.out.println("Size of matrix (axb)");
-      String size = reader.next();
-      Matrix C = new Matrix("C", size, random);
-      C.inverse().printMatrix();
-      //Matrix reduced = C.rowReduce();
-      //reduced.printMatrix();
-    } */
-    
-    Matrix A = new Matrix("A", "9x9", false);
-    A.printMatrix();
-    //A.solveSudoku();
-    A.printAsSudoku();
-    
-    /* Matrix E = new Matrix("E", "2x2", false);
-    Matrix F = new Matrix("F", "2x2", false);
-    E.multiplyMatrices(F, "EF").printMatrix(); */
-
+    A.sort("c", 4).printMatrix();
   }
 
   public static double[] addingFractions(double num1, double num2) {
